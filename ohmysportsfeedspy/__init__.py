8929d559a436a74fa4f097407b340f357c9c7d41
Brad Barkhouse <brad.barkhouse@gmail.com>
2017-06-02 22:35:28 -0300

    De-coupled the API version from teh main class and wrapper.  Simplified the call structure and example.  Included commandline script to test with player_gamelogs feed.

---
+++ ohmysportsfeedspy/__init__.py
@@ -0,0 +1,7 @@
+### Import MySportsFeeds class(es) upon initiation of the program
+from .MySportsFeeds_API import MySportsFeeds
+from .v1_0 import API_v1_0
+
+from pkg_resources import get_distribution
+
+__version__ = "0.1.0" #get_distribution('ohmysportsfeedspy').version
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ohmysportsfeedspy',
+    version='0.1.0',
+    author = ['Brad Barkhouse', 'MySportsFeeds'],
+    packages=['ohmysportsfeedspy'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
