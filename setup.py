fb3c119dad53d378004011560373a22b5135e36d
Brad Barkhouse <brad.barkhouse@gmail.com>
2017-06-04 21:49:39 -0300

    Adding author_email as recommended by pip.

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from distutils.core import setup
 setup(
     name='ohmysportsfeedspy',
     packages=['ohmysportsfeedspy'],
-    version='0.1.0',
+    version='0.1.1',
     author = ['Brad Barkhouse', 'MySportsFeeds'],
+    author_email='brad.barkhouse@mysportsfeeds.com',
     url='https://github.com/MySportsFeeds/mysportsfeeds-python',
     license='MIT',
     description='A Python wrapper for the MySportsFeeds sports data API'
