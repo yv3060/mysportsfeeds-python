24b723f6c35ca3f439555ba256380b7bca9c005d
Brad Barkhouse <brad.barkhouse@gmail.com>
2017-06-04 21:32:17 -0300

    Preparing for PyPI package installation.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from distutils.core import setup
 
 setup(
     name='ohmysportsfeedspy',
+    packages=['ohmysportsfeedspy'],
     version='0.1.0',
     author = ['Brad Barkhouse', 'MySportsFeeds'],
-    packages=['ohmysportsfeedspy'],
+    url='https://github.com/MySportsFeeds/mysportsfeeds-python',
     license='MIT',
+    description='A Python wrapper for the MySportsFeeds sports data API',
     long_description=open('README.md').read(),
 )
