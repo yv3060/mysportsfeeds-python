fe5a980dd36c24008efe8e4900a675b568e5fb9d
Brad Barkhouse <brad.barkhouse@gmail.com>
2017-06-08 10:19:46 -0300

    Increment patch version to match submission to PyPI for pip install.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ohmysportsfeedspy',
     packages=['ohmysportsfeedspy'],
-    version='0.1.2',
+    version='0.1.3',
     author = ['Brad Barkhouse', 'MySportsFeeds'],
     author_email='brad.barkhouse@mysportsfeeds.com',
     url='https://github.com/MySportsFeeds/mysportsfeeds-python',
