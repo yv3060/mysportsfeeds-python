ea922b362dee9176934eca2bd3d06b07ea0c5833
Brad Barkhouse <brad.barkhouse@gmail.com>
2018-08-16 17:26:37 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from ohmysportsfeedspy import MySportsFeeds
 
 Data_query = MySportsFeeds('1.2',verbose=True)
-Data_query.authenticate('7f38bf2c-0bb5-4308-85b0-6057b8', 'Dk30RQHT')
+Data_query.authenticate('YOUR_API_KEY', 'YOUR_ACCOUNT_PASSWORD')
 Output = Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='player_gamelogs',format='json',player='stephen-curry')
 
 print(Output)
